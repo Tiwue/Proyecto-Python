23852a185090e8f5b23d2871a153d9d512618142
Tiwue <72821653+Tiwue@users.noreply.github.com>
2020-11-06 13:38:45 -0600

    Add files via upload
    terminado

--- Proyecto Python Basico/app.py
+++ Proyecto Python Basico/app.py
@@ -1,1 +1,19 @@
 #iniciar
+intentos=2
+
+def jugar(intentos):
+
+    respuesta = input("\n¿Dime de que  color es la fruta en que estoy pensando?: \n")
+    if str(respuesta) != "naranja" :
+        if intentos>0:
+            intentos = intentos-1
+            print("\n Fallaste te quedan "+ str(intentos+1)+" intentos.\n Intentalo de nuevo:")
+            jugar(intentos)
+        else:
+            print("\n Perdiste el juego se acabaron los intentos :(\n")
+    elif str(respuesta) =="naranja" :
+        
+        print("|  Ganaste era naranja | :) ")     
+       
+jugar(intentos)           
+
